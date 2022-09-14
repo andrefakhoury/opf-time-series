193c79e3a4a0d02b9f3fb7ec3e6b9e9822d10ea2
André Fakhoury <andfakhoury@gmail.com>
2022-09-14 13:49:19 -0300

    Update visualization and OPF

--- src/OPFClassifier.py
+++ src/OPFClassifier.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import defaultdict
 from dtaidistance import dtw
+from operator import itemgetter
 import heapq
 
 """Union Find structure with Path Compression + Merge by Size"""
@@ -27,23 +28,24 @@ class DSU:
             u, v = v, u
         self.par[u] = v
         self.siz[v] += self.siz[u]
-        
+
 """Optimum Path Forest Classifier"""
-class OptimumPathForestClassifier:
+class OptimumPathForestClassifier:    
     def __init__(self, cost='euclidean-distance'):
         available_cost_functions = {
             'euclidean-distance': lambda x, y: np.linalg.norm(x - y),
             'manhattan-distance': lambda x, y: np.sum(np.abs(x - y)),
-            'dtw-distance': lambda x, y: dtw.distance_fast(x, y, use_pruning=True)
+            'dtw-distance': lambda x, y: dtw.distance_fast(x, y, window=100, use_pruning=True)
         }
-        assert cost in available_cost_functions.keys(), f"Invalid cost function. Should be one of {available_cost_functions.keys()}"
-        self.F = available_cost_functions[cost]            
+        assert cost in available_cost_functions.keys(),\
+            f"Invalid cost function. Should be one of {available_cost_functions.keys()}"
+        self.F = available_cost_functions[cost]
     
     def fit(self, X_, Y_):
         n = len(Y_)
-        self.X = np.array(X_, copy=True)
-        self.labels = np.ones(n) * -1
-        Y = np.array(Y_, copy=True)
+        self.X = np.array(X_, copy=True, dtype=float)
+        self.label = np.ones(n, dtype=int) * -1
+        Y = np.array(Y_, copy=True, dtype=int)
         
         # First of all, builds the graph
         self.adj = defaultdict(list)
@@ -52,7 +54,7 @@ class OptimumPathForestClassifier:
             self.adj[u] = [(v, self.F(self.X[u], self.X[v])) for v in range(n)]
             self.edges += [(w, u, v) for v, w in self.adj[u]]
         
-        # Runs MST to choose PROTOTYPES (seed vertices)
+        # Runs MST (Kruskal) to choose PROTOTYPES (seed vertices)
         self.prototypes = []        
         self.edges.sort()
         dsu = DSU(n)
@@ -61,12 +63,13 @@ class OptimumPathForestClassifier:
                 dsu.merge(u, v)
                 if Y[u] != Y[v]:
                     self.prototypes += [u, v]
-        self.prototypes = np.unique(self.prototypes)                
+        self.original_prototypes = list(dict.fromkeys(self.prototypes))
+        self.prototypes = np.unique(self.prototypes)
         
         # Run multisourced dijkstra on prototypes to get the cost
         self.cost = np.ones(n) * np.inf
         self.cost[self.prototypes] = 0
-        self.labels[self.prototypes] = Y[self.prototypes]
+        self.label[self.prototypes] = Y[self.prototypes]
         
         pq = [[0., u] for u in self.prototypes]
         heapq.heapify(pq)
@@ -77,21 +80,26 @@ class OptimumPathForestClassifier:
             for v, w in self.adj[u]:
                 if self.cost[v] > max(u_w, w):
                     self.cost[v] = max(u_w, w)
-                    self.labels[v] = self.labels[u]
+                    self.label[v] = self.label[u]
                     heapq.heappush(pq, [self.cost[v], v])
+        self.ordered_nodes = [(u, self.cost[u]) for u in range(n)]
+        self.ordered_nodes.sort(key=itemgetter(1))
                     
     def _classify_one_vertex(self, x):
-        b, y = np.inf, -1
-        for i in range(len(self.X)):
-            nb = max(self.F(self.X[i], x), self.cost[i])
-            if nb < b:
-                b, y = nb, self.labels[i]
-        assert(y != -1)
-        return y
+        best_index, best_cost = self.ordered_nodes[0]
+        best_cost = max(best_cost, self.F(self.X[best_index], x))
+        best_label = self.label[best_index]
+        
+        for i in range(1, len(self.X)):
+            cur_index, cur_cost = self.ordered_nodes[i]
+            if cur_cost > best_cost:
+                break
+            cur_cost = max(cur_cost, self.F(self.X[cur_index], x))
+            cur_label = self.label[cur_index]
+            if cur_cost < best_cost:
+                best_index, best_cost, best_label = cur_index, cur_cost, cur_label
+        return best_label
     
     def classify(self, X_):
         X_train = np.array(X_, copy=True)
-        Y_pred = []
-        for x in X_train:
-            Y_pred.append(self._classify_one_vertex(x))
-        return Y_pred
+        return [self._classify_one_vertex(x) for x in X_train]
